2022f52041c00a55d2fa69e77f36a8d686573c35
LINLIN345 <houxiaolin345@163.com>
2022-07-25 16:00:23 +0800

    add read file

--- products.py
+++ products.py
@@ -1,21 +1,35 @@
+# read file and split
+products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        # use continue to jump to next circle in for
+        if "products, price" in line:
+            continue
+        product, price = line.strip().split(',')
+        p = [product, price]
+        products.append(p)
+print(products)
+
+
 # if don't known the time of loop , chose while loop
-product = []
+# input product and price
+products = []
 while True:
     name = input("Please input the name of product")
     if name == "q":
         break
     price = input("Please input the price")
     p = [name, price]
-    product.append(p)
-print(product)
+    products.append(p)
+print(products)
 
-# for loop to find every one 
-for p in product:
+# for loop to find every one
+for p in products:
     print("The price of ", p[0], "is", p[1])
 
-# save the product to csv file
+# save and write the product to csv file
 # add colum name
 with open("products.csv", "w", encoding="utf-8") as f:
     f.write('products, price\n')
-    for p in product:
+    for p in products:
         f.write(p[0] + "," + p[1] + "\n")
