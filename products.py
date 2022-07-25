7338df18460df9fc2e3b890a995ce0f876c188b2
LINLIN345 <houxiaolin345@163.com>
2022-07-25 15:04:18 +0800

    add header

--- products.py
+++ products.py
@@ -11,4 +11,11 @@ print(product)
 
 # for loop to find every one 
 for p in product:
-    print("The price of ",p[0],"is", p[1])
+    print("The price of ", p[0], "is", p[1])
+
+# save the product to csv file
+# add colum name
+with open("products.csv", "w", encoding="utf-8") as f:
+    f.write('products, price\n')
+    for p in product:
+        f.write(p[0] + "," + p[1] + "\n")
