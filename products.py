4e9d8b05d01c691d6dfa505d43f931353e9d6ea9
LINLIN345 <houxiaolin345@163.com>
2022-07-25 16:31:54 +0800

    add check file

--- products.py
+++ products.py
@@ -1,15 +1,22 @@
-# read file and split
-products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        # use continue to jump to next circle in for
-        if "products, price" in line:
-            continue
-        product, price = line.strip().split(',')
-        p = [product, price]
-        products.append(p)
-print(products)
 
+import os
+products = []
+
+# check the file if exit
+# read file and split
+if os.path.isfile('products.csv'):
+    print("You have found this file!")
+    with open('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            # use continue to jump to next circle in for
+            if "products, price" in line:
+                continue
+            product, price = line.strip().split(',')
+            p = [product, price]
+            products.append(p)
+    print(products)
+else:
+    print('Can not find this file')
 
 # if don't known the time of loop , chose while loop
 # input product and price
